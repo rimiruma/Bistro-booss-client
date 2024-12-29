08a6649c408937235202318a8da8dcbc140a8134
rimiruma <rimiruma12@gmail.com>
2024-12-29 09:42:21 +0600

    added category done

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
 
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white max-w-screen-lg">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -3,9 +3,9 @@ import { Carousel } from 'react-responsive-carousel';
 import img1 from '../../../assets/home/01.jpg'
 import img2 from '../../../assets/home/02.jpg'
 import img3 from '../../../assets/home/03.png'
-// import img4 from '../../../assets/home/04.jpg'
-// import img5 from '../../../assets/home/05.png'
-// import img6 from '../../../assets/home/06.png'
+import img4 from '../../../assets/home/04.jpg'
+import img5 from '../../../assets/home/05.png'
+import img6 from '../../../assets/home/06.png'
 
 const Banner = () => {
     return (
@@ -21,6 +21,18 @@ const Banner = () => {
                 </div>
                 <div>
                     <img src={img3} />
+                    
+                </div>
+                <div>
+                    <img src={img4} />
+                    
+                </div>
+                <div>
+                    <img src={img5} />
+                    
+                </div>
+                <div>
+                    <img src={img6} />
                     
                 </div>
             </Carousel>
