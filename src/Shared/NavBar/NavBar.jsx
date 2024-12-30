963a8de615220b9c9c3b9a7ee5590a37ea1ef9ad
rimiruma <rimiruma12@gmail.com>
2024-12-30 22:42:08 +0600

    added order done

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -5,7 +5,8 @@ const NavBar = () => {
     const links = <>
         <li className="text-white"><Link to="/">Home</Link></li>
         <li><Link to="/menu">Our Menu</Link></li>
-        <li><a>Item 3</a></li>
+        <li><Link to="/order">Our Order</Link></li>
+        
     </>
 
     return (
---
+++ src/components/FoodCard/FoodCard.jsx
@@ -0,0 +1,25 @@
+const FoodCard = ({item}) => {
+    const {name, image, price, recipe} = item;
+
+    return (
+        <div>
+            <div className="card bg-base-100 shadow-xl h-[400px]">
+                <figure>
+                    <img className=""
+                        src={image}
+                        alt="Shoes" />
+                </figure>
+                <p className="bg-slate-900 text-white absolute right-0 mr-4 mt-4 px-4">${price}</p>
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
+                    <p>{recipe}</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Add To cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FoodCard;
